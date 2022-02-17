3462896b1fd5205cb4f0a0cd76e259b348137e86
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-02-17 05:38:49 +0300

    edit prodconfig

--- config.py
+++ config.py
@@ -5,38 +5,27 @@ class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/blogs'
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-#   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace("://", "ql://", 1)
-
-
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/pitches_test'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/comment_test'
-
-
+    pass
 class DevConfig(Config):
     '''
     Development configuration child class 
     Args:
       Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/pitches'
     DEBUG = True
 
 
 config_options = {
     'development': DevConfig,
     'production': ProdConfig,
-    'test': TestConfig
 }
