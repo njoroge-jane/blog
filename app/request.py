3bb0316218472984afd0650f709be03612c21812
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-02-17 06:50:58 +0300

    edit styles

--- app/request.py
+++ app/request.py
@@ -9,20 +9,20 @@ def configure_request(app):
 
     base_url = app.config['QUOTE_BASE_URL']
 
-def random():
-    get_random_quote = base_url
+def quote():
+    get_quote = base_url
 
-    with urllib.request.urlopen(get_random_quote) as url:
-        get_random_data = url.read()
-        get_random_response = json.loads(get_random_data)
+    with urllib.request.urlopen(get_quote) as url:
+        get_data = url.read()
+        get_response = json.loads(get_data)
 
-        random_results = None
+        quote_results = None
 
-        if get_random_response:
-            random_results_list = get_random_response
-            random_results = process_results(random_results_list)
+        if get_response:
+            results_list = get_response
+            qoute_results = process_results(results_list)
 
-        return random_results   
+        return quote_results   
 
 def process_results(result_list):
     author = result_list.get('author')
